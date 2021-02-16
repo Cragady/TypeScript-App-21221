8c2d6c8dcf04f2490cb14fb286c23b856f541662
Cragady <pinkdotdreamer@gmail.com>
2021-02-16 11:36:54 -0700

    Fixed some missing dependencies. Moving along with setup with contexts, resolvers, and types.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,31 @@
+import "reflect-metadata";
 import 'dotenv/config';
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 // import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
-import express from 'express';
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => { 
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
     
     const app = express(),
+        apolloServer = new ApolloServer({ 
+            schema: await buildSchema({
+                resolvers: [ HelloResolver, PostResolver ],
+                validate: false
+            }),
+            context: () => ({ em: orm.em })
+        }),
         PORT = process.env.PORT || 3000;
 
-    app.get('/', (_, res) => {
-        res.send("Hello");
-    });
+
+    apolloServer.applyMiddleware({ app });
 
     app.listen(PORT, () => {
         console.log(`Server up on http://localhost:${PORT}`);
