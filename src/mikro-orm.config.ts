e001e192f805f5dd853beac470fe20a4a09d21a6
Cragady <pinkdotdreamer@gmail.com>
2021-02-18 11:51:49 -0700

    Added create migration script to package.json.
    Added dotenv/config to ensure migration script worked and added User entity to mikro-orm.config.ts. Also created user entity.
    
    Created new migration.
    
    Added Mutations to post.ts in resolvers.
    
    Created user.ts in resolvers.
    
    Added new dependency: argon2.

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
@@ -8,7 +10,7 @@ export default {
         path: path.join(__dirname, "./migrations"),
         pattern: /^[\w-]+\d+\.[tj]s$/
     },
-    entities: [Post],
+    entities: [Post, User],
     dbName: "lireddit",
     type: "postgresql",
     user: process.env.DB_USER,
